0b3f683b45f08370c1088f2b8c9c563bf0504bf9
lovemagician <ddiddibboddiddibbo@gmail.com>
2023-10-04 23:24:40 +0900

    add join

--- src/init.js
+++ src/init.js
@@ -1,11 +1,11 @@
 import "./db";
 import "./models/Video";
-import app from "./server"
-
+import "./models/User";
+import app from "./server";
 
 const PORT = 8080;
 
-const handleListening = () => 
+const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🚀`);
 
-app.listen(PORT, handleListening); 
+app.listen(PORT, handleListening);
