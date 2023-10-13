71d033eb9d5eced3551151e396b276f6e1851a99
lovemagician <ddiddibboddiddibbo@gmail.com>
2023-10-14 05:33:52 +0900

    add logout

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,17 +1,16 @@
 import express from "express";
 import {
   edit,
-  remove,
   logout,
   see,
   startGithubLogin,
+  finishGithubLogin,
 } from "../controllers/userControllers";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
-userRouter.get("/remove", remove);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get("/:id", see);
